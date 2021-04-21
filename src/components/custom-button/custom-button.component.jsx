c5dc215930b3a3804566d4605839cc8ed02c4796
Daniel Wallace <wallacedaniel999@gmail.com>
2021-04-21 13:30:53 -0700

    updated new shop data and added cart utils

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSign, ...otherProps}) => (
-    <button className={`${isGoogleSign? 'google-sign-in' : ''} custom-button`} {...otherProps} >
+const CustomButton = ({ children, isGoogleSign, inverted, ...otherProps}) => (
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSign? 'google-sign-in' : ''} custom-button`} {...otherProps} >
       {children}
     </button>
 );
